0920d6dc5d8379a0a4b10d0b83a34e6a75baa37c
Jacob <jacob_fafiolu@hotmail.co.uk>
2024-09-10 14:24:43 +0200

    Added framework

--- Package.swift
+++ Package.swift
@@ -5,19 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "LiveUI",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LiveUI",
-            targets: ["LiveUI"]),
+            targets: ["LiveUIFramework"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "LiveUI"),
-        .testTarget(
-            name: "LiveUITests",
-            dependencies: ["LiveUI"]),
+        .binaryTarget(name: "LiveUIFramework", path: "./Sources/LiveUIFramework.xcframework")
     ]
 )
