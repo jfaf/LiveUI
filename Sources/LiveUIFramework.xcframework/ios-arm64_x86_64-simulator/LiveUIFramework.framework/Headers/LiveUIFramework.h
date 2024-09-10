cc66cae3bfb28d66182898bb0872a1400860a844
Jacob <jacob_fafiolu@hotmail.co.uk>
2024-09-10 14:24:21 +0200

    Added framework

---
+++ Sources/LiveUIFramework.xcframework/ios-arm64_x86_64-simulator/LiveUIFramework.framework/Headers/LiveUIFramework.h
@@ -0,0 +1,18 @@
+//
+//  LiveUIFramework.h
+//  LiveUIFramework
+//
+//  Created by user on 10/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LiveUIFramework.
+FOUNDATION_EXPORT double LiveUIFrameworkVersionNumber;
+
+//! Project version string for LiveUIFramework.
+FOUNDATION_EXPORT const unsigned char LiveUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveUIFramework/PublicHeader.h>
+
+
